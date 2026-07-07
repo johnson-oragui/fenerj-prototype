e25a23774ba70c4f994a9b6b146f47bac3929621
johnson-oragui <johnson.oragui@gmail.com>
2026-07-07 17:42:32 +0100

    feat: Add new assets and update asset paths

--- public/path/asset-paths.ts
+++ public/path/asset-paths.ts
@@ -1,2 +1,4 @@
 export const APP_LOGO = '/fenerj-logo.jpg';
 export const MD_IMAGE = '/Fenerj_MD.jpg';
+export const DATA_ANALYST_IMAGE = '/fenerj_data_Analyst.jpg';
+export const FULLSTACK_IMAGE = '/fenerj_fullstack_developer.png';
