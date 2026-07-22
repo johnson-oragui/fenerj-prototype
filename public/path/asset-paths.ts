26e66860c6fc35e51246903d3071a2776e589fb3
johnson-oragui <johnson.oragui@gmail.com>
2026-07-22 06:36:52 +0100

    chore: Change director image

--- public/path/asset-paths.ts
+++ public/path/asset-paths.ts
@@ -2,6 +2,6 @@ export const APP_LOGO = '/fenerj-logo.jpg';
 export const MD_IMAGE = '/fenerj_md_portrait.jpg';
 export const DATA_ANALYST_IMAGE = '/fenerj_data_Analyst.jpg';
 export const FULLSTACK_IMAGE = '/fenerj_fullstack_developer.png';
-export const DIRECTOR_IMAGE = '/fenerj_director.jpg';
+export const DIRECTOR_IMAGE = '/fenerj_director_2.jpg';
 export const DEVELOPER_IMAGE = '/joseph_software.jpg';
 export const TECHNICAL_LEAD_IMAGE = '/emmanuel_sunday_ogbodo_1.jpg';
